16152c1ea3dce4ba30ddc113695bbf098fb3ac1b
eufanzky <eufanzky@gmail.com>
2024-07-02 20:06:46 -0400

    changed file order

---
+++ src/app/main.ts
@@ -0,0 +1,10 @@
+import 'angular';
+import 'angular-resource';
+
+
+import './app.main'
+import './controllers';
+import './directives';
+import './filters';
+import './services';
+import './app.routes'
